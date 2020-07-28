1b93931f68b103e750505526e30da52978ccd64d
Lucas <lucasdesousa19@hotmail.com>
2020-07-27 21:57:38 -0300

    dompdf

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require './vendor/autoload.php';
+
+use Dompdf\Dompdf;
+
+
+$dompdf = new Dompdf();
+
+// $dompdf->loadHtml()
+
+ob_start();
+require __DIR__ . '/contents/frontend.php';
+$dompdf->loadHtml(ob_get_clean());
+// $pdf  = ob_get_clean();
+
+$dompdf->setPaper('A4');
+
+$dompdf->render();
+
+$dompdf->stream('frontend.pdf', ['Attachment' => false]);
+
+
+
+?>

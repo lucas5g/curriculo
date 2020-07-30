7b84a3593a8e1a316739f3d20d42b654fb9fc0c8
Lucas <lucasdesousa19@hotmail.com>
2020-07-30 16:25:45 -0300

    make pdf backend

---
+++ contents/backend.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500&display=swap" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="styles.css"> -->
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            /* font-family: Arial, Helvetica, sans-serif; */
+            background-color: white;
+            font-family: 'Roboto', sans-serif;
+            /* line-height: 1.5; */
+            font-size: 15px;
+            text-align: justify;
+            height: 100vh;
+
+        }
+
+        #container {
+            /* width: 21cm; */
+            background-color: white;
+            /* padding: 2cm; */
+            padding-top: 2cm;
+            padding-left: 2cm;
+            padding-right: 2cm;
+
+            margin: 0 auto;
+
+        }
+
+        div.section {
+            padding: 0;
+            margin-bottom: 20px;
+
+            width: auto;
+
+        }
+
+        div.section ul {
+            /* padding-left: 20px; */
+            list-style: none;
+
+        }
+
+        div.section ul li {
+            margin-bottom: 5px;
+        }
+
+        div .section .row {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        h1 {
+            margin-bottom: 10px;
+            font-size: 30pt;
+        }
+
+        h2 {
+            /* text-align: center; */
+            font-size: 15px;
+            margin-bottom: 10px;
+            background-color: #80808040;
+            padding-top:  -4px;
+            padding-left: 3px;
+            padding-bottom: 2px;
+            border-radius: 5px;
+            /* border-bottom: 1px solid grey; */
+        }
+        table{
+            width: 100%;
+         
+        }
+    </style>
+</head>
+
+<body>
+    <div id="container" >
+        <h1>Lucas de Sousa Assunção</h1>
+        <div class="section">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <ul>
+                                <li>Brasileiro, solteiro, 27 anos</li>
+                                <li>Cardoso, Belo Horizonte - MG</li>
+                                <li>(31) 97305-6405</li>
+                            </ul>
+                        </td>
+                        <td style="text-align:right;">
+                            <ul>
+                                <li>lucasdesousa19@hotmail.com</li>
+                                <li><a href="https://github.com/lucas5g" target="_blank"> https://github.com/lucas5g</a></li>
+                                <li> <a href="https://www.linkedin.com/in/lucas-sousa19" target="_blank">
+                                        https://www.linkedin.com/in/lucas-sousa19</a></li>
+                            </ul>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+        <div class="section">
+            <h2> Objetivo </h2>
+            <span>
+                Backend Developer
+            </span>
+        </div>
+        <div class="section">
+            <h2> Formação </h2>
+            <ul>
+                <li>
+                    <strong>Universidade Estácio de Sá</strong><br>
+                    Analista de Desenvolvimento de Sistemas. <br>
+                    2019 - 2021
+                </li>
+                <li>
+                    <strong>EPSA (Escola profissionalizante Santo Agostinho)</strong><br>
+                    Técnico de Informática. <br>
+                    2015 - 2016
+                </li>
+                <li>
+                    <strong>SENAI - CETEL</strong><br>
+                    Técnico em Eletrônica<br>
+                    2012 - 2013
+                </li>
+            </ul>
+        </div>
+        <div class="section">
+            <h2> Experiência </h2>
+            <ul>
+                <li>
+                    <strong>Técnico de Informática</strong><br>
+                    Colégio Agostiniano Frei Carlos Vicuña. <br>
+                    fev. de 2017 - Atualmente <br>
+                    Responsável pela manutenção dos computadores,
+                    atendimento dos usuários e criação de aplicações para setores específicos da empresa.
+                </li>
+                <li>
+                    <strong>Técnico de sistemas de radiocomunicação</strong><br>
+                    Alfacom Serviços e Ltda <br>
+                    fev. de 2013 - mai. de 2016 <br>
+                    Responsável pela programação, manutenção e instalação
+                    dos sistemas de radiocomunicação. Suporte na área de TI na rede local da empresa e instalação de
+                    sistemas de CFTV.
+                </li>
+            </ul>
+        </div>
+        <div class="section">
+            <h2>Competências </h2>
+            <table style="width: 100%">
+                <tbody>
+                    <tr>
+                        <td>
+
+                            <ul>
+                                <li>PHP</li>
+                                <li>JavaScript</li>
+                                <li>Laravel</li>
+                                <li>Bootstrap</li>
+                            </ul>
+                        </td>
+                        <td>
+                            <ul>
+                                <li>ReactJs</li>
+                                <li>HTML5</li>
+                                <li>CSS3</li>
+                                <li>Inglês Básico</li>
+                            </ul>
+                        </td>
+                        <td>
+                            <!-- <ul>
+                                <li>ReactJs</li>
+                                <li>HTML5</li>
+                                <li>CSS3</li>
+                                <li>Inglês Básico</li>
+                            </ul> -->
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <ul>
+
+
+
+            </ul>
+        </div>
+
+
+
+    </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -10,7 +10,9 @@ $dompdf = new Dompdf();
 // $dompdf->loadHtml()
 
 ob_start();
-require __DIR__ . '/contents/frontend.php';
+// require __DIR__ . '/contents/frontend.php';
+require __DIR__ . '/contents/backend.php';
+
 $dompdf->loadHtml(ob_get_clean());
 // $pdf  = ob_get_clean();
 
